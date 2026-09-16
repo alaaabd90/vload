c29484e2ad5b573f393d5623fc63f3d4b2d25086
Hayder <hayderalhijjaj90@gmail.com>
2026-09-16 03:45:37 +0300

    Revert "Update to sing-box testing branch snapshot (1.15.0-alpha.4)"
    This reverts commit fd3d1be99b2b26e691dc5d3d0606f6066ec04751.

--- libcore/platform_box.go
+++ libcore/platform_box.go
@@ -173,14 +173,6 @@ func (w *boxPlatformInterfaceWrapper) CreateBridge(options adapter.BridgeOptions
 	return nil, E.New("not implemented")
 }
 
-func (w *boxPlatformInterfaceWrapper) UsePlatformAutoRedirect() bool {
-	return false
-}
-
-func (w *boxPlatformInterfaceWrapper) CreateAutoRedirect(options adapter.AutoRedirectOptions) (adapter.AutoRedirectSession, error) {
-	return nil, E.New("not implemented")
-}
-
 func (s *boxPlatformInterfaceWrapper) SystemCertificates() []string {
 	return nil
 }
@@ -237,7 +229,7 @@ func (w *boxPlatformInterfaceWrapper) FindConnectionOwner(request *adapter.FindC
 	if packageName != "" {
 		packageNames = []string{packageName}
 	}
-	return &adapter.ConnectionOwner{UserId: uid, PackageNames: packageNames}, nil
+	return &adapter.ConnectionOwner{UserId: uid, AndroidPackageNames: packageNames}, nil
 }
 
 // io.Writer
